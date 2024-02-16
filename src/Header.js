f3d69728e6efe5dbfaa4e64a6201010814052bb3
anjaliydv304 <anjali.ydv304@gmail.com>
2024-02-16 15:17:09 +0530

    header

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+function Header() {
+  return (
+    <div className="header">
+       
+
+       <div className='header__left'>
+         <img src="https://www.flaticon.com/free-icons/linkedin " alts=""/>
+
+
+         <div className="header__search">
+            <SearchIcon/>
+            <input type="text"/>
+        </div>
+       </div>
+       <div className='header__right'>
+           <HeaderOption Icon={HomeIcon} title='Home'/>
+           <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+           <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+           <HeaderOption Icon={ChatIcon} title='Messaging'/>
+           <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+           <HeaderOption avatar={AccountCircleIcon} title='me'/>
+
+       </div>
+    
+    </div>
+  );
+}
+
+export default Header;
