a5102edeb16cf6abf89a158b1202b4334012a711
anjaliydv304 <anjali.ydv304@gmail.com>
2024-02-25 19:06:39 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
-
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import { login, logout, selectUser } from './features/userSlice';
+import {useSelector } from 'react-redux/es/hooks/useSelector';
+import Login from './login';
+import { useDispatch } from 'react-redux';
+import { auth } from './firebase';
+import Widgets from './Widgets';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    auth.onAuthStateChanged(userAuth=>{
+      if(userAuth){
+       dispatch(login({
+        email:userAuth.email,
+        uid:userAuth.uid,
+        displayName:userAuth.displayName,
+        photourl:userAuth.photoURL,
+
+       }))
+      }
+      else{
+        dispatch(logout());
+
+      }
+      
+    })
+  },[])
+
   return (
     <div className="app">
       
-      {/*Header*/}
+      
       <Header/>
 
-      {/*App Body*/}
-        {/*Sidebar*/}
-         {/*Feed*/}
-          {/*Widgets*/}
+      {!user?
+        <Login/>:(
+        <div className="app__body">
+        <Sidebar/>
+        <Feed/>
+        <Widgets/> 
+        </div>
+      )}
       
     </div>
   );
